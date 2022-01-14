c58f17a4ca1c4dc4c1a4ca28d6b9e8cbf963d161
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-13 22:37:37 -0700

    add javascript for hiding start quiz card and showing question

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var startButton = document.querySelector(".start-btn");
+var startCard = document.querySelector("#start-card");
+var questionCard = document.querySelector("#question-card")
+var question = document.querySelector("#question")
+var index = 0
+
+
+var questions = [{
+    title: "Which of the following best describes a Web API?",
+    choices: ["Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.", "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.", "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web.", "Web APIs are low level code (say C or C++) that directly control the computer's GPU or other graphics functions."],
+    answer: "Which of the following would change an element's background to red?"
+}, {
+    title: "What value would we add to setInterval() if we want a function called, myTimer() to run every 3 seconds? ",
+    choices: ["setInterval(myTimer, 3)", "setInterval(myTimer, 30)", "setInterval(myTimer, 3000)", "setInterval(myTimer, 300)"],
+    answer: "setInterval(myTimer, 3000)"
+}, {
+    title: "hich property can you use in order to implement event delegation?",
+    choices: ["event.target", "event.preventDefault()", "event.stopPropagation()", "event.addEventListener()"],
+    answer: "event.target"
+}]
+
+console.log(startButton);
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz() {
+    console.log("Hello World!")
+    startCard.style.display = "none"
+    questionCard.style.display = "block"
+    nextQuestion()
+}
+
+function nextQuestion() {
+    question.textContent = questions[index].title
+
+    // create for loop
+    // create buttons inside for loop
+    // append to DOM
+    // attach onclick function to each button
+    // increment index var inside onclick function
+    // call on next question
+    // create base case to end quiz
+}
