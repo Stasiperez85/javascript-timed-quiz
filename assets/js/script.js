cd70c88bfe0fdc70593881cf726813095c40c0bf
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-14 18:05:24 -0700

    adding loop for answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,23 @@ var startButton = document.querySelector(".start-btn");
 var startCard = document.querySelector("#start-card");
 var questionCard = document.querySelector("#question-card")
 var question = document.querySelector("#question")
+var optionAnswer =  document.querySelector(".option-text")
 var index = 0
 
 
 var questions = [{
     title: "Which of the following best describes a Web API?",
-    choices: ["Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.", "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.", "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web.", "Web APIs are low level code (say C or C++) that directly control the computer's GPU or other graphics functions."],
-    answer: "Which of the following would change an element's background to red?"
+    choices: ["Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.",
+     "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript.", 
+     "Web APIs are not built into the browser by default, and you generally have to retrieve their code and information from somewhere on the Web.", 
+     "Web APIs are low level code (say C or C++) that directly control the computer's GPU or other graphics functions."],
+    answer: "Web APIs are built into your web browser and contain methods that allow us to manipulate a web page via JavaScript."
 }, {
     title: "What value would we add to setInterval() if we want a function called, myTimer() to run every 3 seconds? ",
     choices: ["setInterval(myTimer, 3)", "setInterval(myTimer, 30)", "setInterval(myTimer, 3000)", "setInterval(myTimer, 300)"],
     answer: "setInterval(myTimer, 3000)"
 }, {
-    title: "hich property can you use in order to implement event delegation?",
+    title: "Which property can you use in order to implement event delegation?",
     choices: ["event.target", "event.preventDefault()", "event.stopPropagation()", "event.addEventListener()"],
     answer: "event.target"
 }]
@@ -23,14 +27,19 @@ console.log(startButton);
 startButton.addEventListener("click", startQuiz)
 
 function startQuiz() {
-    console.log("Hello World!")
     startCard.style.display = "none"
     questionCard.style.display = "block"
     nextQuestion()
 }
 
 function nextQuestion() {
-    question.textContent = questions[index].title
+    for (var i = 0; i < questions.length; i++) {
+        question.textContent = questions[index].title;
+        optionAnswer.textContent =  questions[index].choices
+    
+        }
+        
+    }
 
     // create for loop
     // create buttons inside for loop
@@ -39,4 +48,3 @@ function nextQuestion() {
     // increment index var inside onclick function
     // call on next question
     // create base case to end quiz
-}
