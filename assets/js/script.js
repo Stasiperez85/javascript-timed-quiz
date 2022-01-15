69df8b65ba7d46d99109ed2d28c449bd4ef1004f
Anastasia Perez <Stasiperez85@gmail.com>
2022-01-15 10:57:52 -0700

    add answers to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,33 @@
 var startButton = document.querySelector(".start-btn");
+var nextButton = document.querySelector(".next-btn")
 var startCard = document.querySelector("#start-card");
 var questionCard = document.querySelector("#question-card")
-var optionButton = document.querySelector(".btn")
+var optionButton = document.querySelector("#btn")
 var question = document.querySelector("#question")
 var choices = document.querySelector("#choices")
-var index = 0
+var option1 = document.querySelector("#option1");
+var option2 = document.querySelector("#option2");
+var option3 = document.querySelector("#option3");
+var option4 = document.querySelector("#option4");
+var qindex = 0
 
 
-
-function clearStatusClass(element){
-    element.classList.remove("correct")
-    element.classList.remove("wrong")
-}
 var questions = [
     {
         title: "Which of the following is an advantage of using JavaScript?",
-        choiceA: [
-            { text: "Less server interaction", correct: false },
-            { text: "Immediate feedback to the visitors", correct: false },
-            { text: "Increased interactivity", correct: false },
-            { text: "All of the above.", correct: true },
-        ],
+        choiceA: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above."],
+        answer: "All of the above."
     },
     {
-        title: "Which built-in method returns the length of the string?",
-        choiceA: [
-            { text: "length()", correct: true },
-            { text: "size()", correct: false },
-            { text: "index()", correct: false },
-            { text: "None of the above..", correct: false},
-        ],
+        title: "Which of the following type of variable is visible only within a function where it is defined?",
+        choiceA: ["global variable", "local variable", "local variable", "local variable"],
+        answer: "local variable"
     },
     {
-        title: "Which of the following function of String object combines the text of two strings and returns a new string?",
-        choiceA: [
-            { text: "add()", correct: false },
-            { text: "merge()", correct: false },
-            { text: "concat()", correct: true },
-            { text: "append()", correct: false },
-        ],
+        title: "Which built-in method sorts the elements of an array?",
+        choiceA: ["changeOrder(order)", "order()", "sort()", "None of the above."],
+        answer: "sort()"
     },
-
 ]
 
 console.log(startButton);
@@ -54,11 +41,20 @@ function startQuiz() {
 }
 
 function nextQuestion() {
-    for (var i = 0; i < questions.length; i++) {
-        question.textContent = questions[index].title
-    }
-   
+ question.textContent = questions[qindex].title
+ option1.textContent = questions[qindex].choiceA[0]
+ option2.textContent = questions[qindex].choiceA[1]
+ option3.textContent = questions[qindex].choiceA[2]
+ option4.textContent = questions[qindex].choiceA[3]
+
+nextButton.onlcick = ()=>{
+    if(qindex<= questions.length - 1)
+        qindex++;
 }
+}
+
+
+
     // create for loop
     // create buttons inside for loop
     // append to DOM
